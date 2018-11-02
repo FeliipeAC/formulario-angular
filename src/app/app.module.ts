a2d4c0a087288d2172af62847892862c9c923889
feliipeac <feliipecorreiia@gmail.com>
2018-11-02 13:44:39 -0300

    Atualizando app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { HomeComponent } from './home/home.component';
 import { FormComponent } from './form/form.component';
 import { ListFormComponent } from './list-form/list-form.component';
-=======
->>>>>>> origin/master
-
 
 @NgModule({
   declarations: [
-<<<<<<< HEAD
     AppComponent,
     HomeComponent,
     FormComponent,
     ListFormComponent
-=======
-    AppComponent
->>>>>>> origin/master
   ],
   imports: [
     BrowserModule
